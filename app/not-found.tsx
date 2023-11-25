953e0f5446a365abc26d52e0b0455e2092ef5e4a
Nikita Chekalin <n.chekalin@lampalampa.net>
2023-11-25 21:20:13 +0200

    Refactor: refactored not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,12 @@
 import NotFoundIcon from '@icons/not-found.svg'
+import { Stack } from '@share/components/ui'
 import Image from 'next/image'
 
 const NotFound: React.FC = () => {
   return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white'
-      }}
-    >
+    <Stack align='center' justify='center' style={{ backgroundColor: 'white' }}>
       <Image src={NotFoundIcon} alt='Not found page' width={500} height={500} />
-    </div>
+    </Stack>
   )
 }
 
